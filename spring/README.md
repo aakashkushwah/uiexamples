7cc135c4377db2d02c1e4dafd61f284dcb5113f0
akushwah3 <akushwah3@csc.com>
2018-05-28 09:47:59 +0530

    spring example added

---
+++ spring/README.md
@@ -0,0 +1,2 @@
+** Below to refresh the client application if any changes in git are made for properties file
+curl localhost:8889/admin/refresh -d {} -H "Content-Type: application/json"
---
+++ spring/spring-boot-client/src/main/java/com/akushwah/examples/springboot/ControllerExample.java
@@ -0,0 +1,18 @@
+package com.akushwah.examples.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ControllerExample {
+	@Value("${provider-name}")
+	private String name = "nowhere";
+
+	@RequestMapping("/")
+	public String home() {
+		return "This is a dynamic property loaded from " + name;
+	}
+}
