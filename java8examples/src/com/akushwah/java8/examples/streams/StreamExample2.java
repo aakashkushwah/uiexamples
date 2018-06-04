69b3af37e2b4ab4b64567832a356489bb5902992
akushwah3 <akushwah3@csc.com>
2018-06-04 12:23:29 +0530

    Update on Java 8 Examples

--- java8examples/src/com/akushwah/java8/examples/streams/StreamExample2.java
+++ java8examples/src/com/akushwah/java8/examples/streams/StreamExample2.java
@@ -1,7 +1,10 @@
 package com.akushwah.java8.examples.streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamExample2 {
 	public static void main(String args[]) {
@@ -14,19 +17,79 @@ public class StreamExample2 {
 		peoples.add(new Employee(106, "Adam", 22));
 		peoples.add(new Employee(107, "Samy", 224));
 		peoples.add(new Employee(108, "Duke", 29));
+		
 		double average = calculateAverage(peoples);
+		
 		System.out.println("Average age of employees are (classic way) : " + average);
 		average = average(peoples);
 		System.out.println("Average age of employees are (lambda way) : " + average);
 		peoples.forEach(p -> {System.out.println(p.getName());});
 		System.out.println("Total Age: "+total(peoples));
+		
+		System.out.println("***************************************** Map Example");
+		peoples.stream().map(p -> p.getAge()).forEach(i -> System.out.println("Age is "+i));
+		
+		System.out.println("***************************************** Flat Map Example");
+		flatMap();
+		
+		System.out.println("***************************************** Sort Example");
+		sortMap();
+		
+		System.out.println("***************************************** Any Match Example");
+		anyMatch();
+		
+		System.out.println("***************************************** Distinct Example");
+		distinct();
+		
+		System.out.println("***************************************** find Any Example");
+		findAny();
+		
+		System.out.println("***************************************** skip Example");
+		skip();
+		
+		System.out.println("***************************************** Reduce Example");
+		System.out.println(peoples.stream().map(p -> p.getAge()).reduce(0, (i,j) -> (i+j)));
+		System.out.println(peoples.stream().map(p -> p.getAge()).reduce((i,j) -> (i+j)));
+		
+		System.out.println("***************************************** Collect Example");
+		peoples.stream().map(p -> p.getAge()).collect(Collectors.toList()).forEach(System.out::println);
+	}
+	
+	private static void flatMap(){
+		 String[][] data = new String[][]{{"Aakash", "Jagdeep"}, {"Anmol", "Peeyush"}, {"Rajat", "Prateek"}};
+		 Arrays.stream(data).flatMap(x -> Arrays.stream(x)).filter(s -> s.startsWith("A")).forEach(System.out::println);
+	}
+	
+	private static void sortMap(){
+		 String[] data = new String[]{"Aakash", "Jagdeep", "Anmol", "Peeyush", "Rajat", "Prateek"};
+		 Arrays.stream(data).sorted((x,y) -> ((Integer)x.length()).compareTo(y.length())).forEach(System.out::println);
+	}
+	
+	private static void anyMatch(){
+		 String[] data = new String[]{"Aakash", "Jagdeep", "Anmol", "Peeyush", "Rajat", "Prateek"};
+		 System.out.println(Arrays.stream(data).anyMatch(s -> s.contains("ee")));
+	}
+	
+	private static void distinct(){
+		 String[] data = new String[]{"A", "B", "C", "D", "A", "B"};
+		 Arrays.stream(data).distinct().forEach(System.out::println);
+	}
+	
+	private static void findAny(){
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+	    Optional<Integer> result = list
+	      .stream().parallel()
+	      .filter(num -> num < 4).findAny();
+	    result.ifPresent(System.out::println);
+	}
+	
+	private static void skip(){
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+	    list.stream().skip(2).forEach(System.out::println);
+	    System.out.println("Limit");
+	    list.stream().parallel().limit(2).forEach(System.out::println);
 	}
 
-	/**
-	 * * Java Method to calculate average from a list of object without using *
-	 * lambdas, doing it on classical java way. * @param employees * @return
-	 * average age of given list of Employee
-	 */
 	private static double calculateAverage(List<? extends Employee> employees) {
 		int totalEmployee = employees.size();
 		double sum = 0;
