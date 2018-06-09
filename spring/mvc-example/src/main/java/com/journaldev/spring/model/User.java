981d36e6a59d6d53948ba13dfa7286e1f020b9e6
akushwah <akushwah@192.168.0.109>
2018-06-10 00:40:53 +0530

    mvc added

---
+++ spring/mvc-example/src/main/java/com/journaldev/spring/model/User.java
@@ -0,0 +1,16 @@
+package com.journaldev.spring.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
