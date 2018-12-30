d1bfa48ea49b41e270f900dcd9b2de6344778b29
Aakash Kushwah <akushwah@Aakashs-MacBook-Pro.local>
2018-12-30 14:18:54 +0530

    template learning 2

--- angularcode/hello-world/src/app/contact-form/contact-form.component.ts
+++ angularcode/hello-world/src/app/contact-form/contact-form.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactMethods=[
+    {id:1, name:"Email"},
+    {id:2, name:"Phone"},
+  ];
 
-  log(firstName){
-    console.log(firstName);
+  log(x){
+    console.log(x);
+  }
+
+  submit(f){
+    console.log(f); 
   }
 }
